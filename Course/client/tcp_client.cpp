26f3cadb61ee93ed20b93daa04fea2911c7f1074
kyarovyi <uchiprogu@gmail.com>
2023-05-08 12:43:13 +0200

    add send data from client and waiting to recive its

--- Course/client/tcp_client.cpp
+++ Course/client/tcp_client.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include "../network_utils.h"
 
 #define DEST_PORT            40000
@@ -6,12 +7,29 @@
 
 using namespace std;
 
+struct  test_struct_t
+{
+    unsigned int a;
+    unsigned int b;
+};
+
+typedef struct result_struct_{
+
+    unsigned int c;
+
+} result_struct_t;
+
 int main(int argc, char **argv) {
+    test_struct_t client_data;
+    result_struct_t result;
     int sockfd = 0;
+    int sent_recv_bytes = 0;
     struct sockaddr_in serv_addr;
+    socklen_t addr_len = 0;
     uint32_t ip_addr;
     uint16_t port_no;
 
+    addr_len = sizeof(sockaddr_in);
 
     if(argc <= 1){
         printf("Warning: usage %s hostname %s port %d\n", argv[0], SERVER_IP_ADDRESS, DEST_PORT);
@@ -40,7 +58,35 @@ int main(int argc, char **argv) {
     	printf("Error: connection failed, error no %d\n", if_connected);
     	exit(0);
     }
-    scanf("\n");
+    printf("intput a number: ");
+    scanf("%u",&client_data.a);
+    printf("intput b number: ");
+    scanf("%u", &client_data.b);
+
+	if(client_data.a == 0 && client_data.b == 0) {
+		close(sockfd);
+		exit(0);
+	}
+    sent_recv_bytes = sendto(sockfd,
+                             &client_data,
+                             sizeof(test_struct_t),
+                             0,
+                             reinterpret_cast<sockaddr*>(&serv_addr),
+                             addr_len);
+    
+    printf("No of bytes sent = %d\n", sent_recv_bytes);
+    printf("Waiting for response:\n");
+
+    sent_recv_bytes = recvfrom( sockfd,
+                                &result,
+                                sizeof(result_struct_t),
+                                0,
+                                reinterpret_cast<sockaddr*>(&serv_addr),
+                                &addr_len);
+
+    printf("No of bytes recvd = %d\n", sent_recv_bytes);
+    
+    printf("Result recvd = %u\n", result.c);
 
     return 0;
 }
